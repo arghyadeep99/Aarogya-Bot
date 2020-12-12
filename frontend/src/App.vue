8a70c94ccb731d6c195c86f6ced9e50ca919ecc2
Neelansh Mathur <53081208+neelansh15@users.noreply.github.com>
2020-12-13 02:25:24 +0530

    Added frontend folder for Vue

---
+++ frontend/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+    <h1>Aarogya Bot</h1>
+    <VueBotUI
+      :messages="data"
+      :options="botOptions"
+      @msg-send="messageSendHandler"
+    />
+  </div>
+</template>
+
+<script>
+import { VueBotUI } from 'vue-bot-ui'
+
+export default {
+  name: 'App',
+  components: {
+    VueBotUI
+  },
+  data(){
+    return{
+      data: [],
+      botOptions:{
+        botTitle: 'Aarogya Bot'
+      }
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
