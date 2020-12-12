fe23d75a7b3894aec2fce92529eab9c88fe9a1fa
Neelansh Mathur <53081208+neelansh15@users.noreply.github.com>
2020-12-13 02:58:06 +0530

    API: Resolved CORS in flask

--- api/main.py
+++ api/main.py
@@ -1,12 +1,16 @@
 from flask import Flask, request
+from flask_cors import CORS, cross_origin
+
 from Logic import ChatBotGraph
 import json
 
 app = Flask(__name__)
+CORS(app)
+
+handler = ChatBotGraph()
 
 @app.route("/")
 def home():
-    handler = ChatBotGraph()
     search_term = request.args.get('search', '')
 
     data = {
