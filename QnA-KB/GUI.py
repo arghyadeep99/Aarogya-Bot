f8fa5dcbfae50a44528081097229368fdc923737
Jayesh Nirve <nitinnirve@gmail.com>
2020-12-13 03:16:50 +0530

    update tkinter stuff

--- QnA-KB/GUI.py
+++ QnA-KB/GUI.py
@@ -1,11 +1,17 @@
 from question_classifier import *
 from question_parser import *
 from answer_search import *
+import tkinter.messagebox
 
 from tkinter import *
 from PIL import ImageTk, Image
 import time
+import pyttsx3
+import threading
 
+
+saved_username = ["You"]
+window_size="400x400"
 
 class ChatBotGraph:
 	def __init__(self):
@@ -27,61 +33,335 @@ class ChatBotGraph:
 			return answer
 		else:
 			return '\n'.join(final_answers)
-		
-def main():
-	handler = ChatBotGraph()  
-	def sendMsg():
-		strMsg = 'User:' + time.strftime("%Y-%m-%d %H:%M:%S",
-									  time.localtime()) + '\n'
-		txtMsgList.insert(END, strMsg, 'greencolor')
-		txtMsgList.insert(END, txtMsg.get('0.0', END))
-		text = txtMsg.get('0.0', END)
-		txtMsg.delete('0.0', END)
-		print ("text is "+text)
-		
-		text2 = handler.chat_main(text) + '\n '
-		strMsg2 = 'Aarogya Bot:' + time.strftime("%Y-%m-%d %H:%M:%S",
-									  time.localtime()) + '\n'
-		txtMsgList.insert(END, strMsg2, 'greencolor')
-		txtMsgList.insert(END, text2)
-										
 
-	def cancelMsg():
-		txtMsg.delete('0.0', END)
+class ChatInterface(Frame):
+
+    def __init__(self, master=None):
+        Frame.__init__(self, master)
+        self.master = master
+
+        #sets default bg for top level windows
+        self.tl_bg = "#EEEEEE"
+        self.tl_bg2 = "#EEEEEE"
+        self.tl_fg = "#000000"
+        self.font = "Verdana 10"
+
+        menu = Menu(self.master)
+        self.master.config(menu=menu, bd=5)
+# Menu bar
+
+    # File
+        file = Menu(menu, tearoff=0)
+        menu.add_cascade(label="File", menu=file)
+       # file.add_command(label="Save Chat Log", command=self.save_chat)
+        file.add_command(label="Clear Chat", command=self.clear_chat)
+      #  file.add_separator()
+        file.add_command(label="Exit",command=self.chatexit)
+
+    # Options
+        options = Menu(menu, tearoff=0)
+        menu.add_cascade(label="Options", menu=options)
+
+        # username
+       
+
+        
+
+        # font
+        font = Menu(options, tearoff=0)
+        options.add_cascade(label="Font", menu=font)
+        font.add_command(label="Default",command=self.font_change_default)
+        font.add_command(label="Times",command=self.font_change_times)
+        font.add_command(label="System",command=self.font_change_system)
+        font.add_command(label="Helvetica",command=self.font_change_helvetica)
+        font.add_command(label="Fixedsys",command=self.font_change_fixedsys)
+
+        # color theme
+        color_theme = Menu(options, tearoff=0)
+        options.add_cascade(label="Color Theme", menu=color_theme)
+        color_theme.add_command(label="Default",command=self.color_theme_default) 
+        color_theme.add_command(label="Grey",command=self.color_theme_grey) 
+        color_theme.add_command(label="Blue",command=self.color_theme_dark_blue) 
+        color_theme.add_command(label="Torque",command=self.color_theme_turquoise)
+        color_theme.add_command(label="Hacker",command=self.color_theme_hacker)
 
 
-	t = Tk()
-	t.title('Aarogya Bot')
+      
+        help_option = Menu(menu, tearoff=0)
+        menu.add_cascade(label="Help", menu=help_option)
+        #help_option.add_command(label="Features", command=self.features_msg)
+        help_option.add_command(label="About Aarogya-Bot", command=self.msg)
+        help_option.add_command(label="Develpoers", command=self.about)
 
-	frmLT = Frame(width=500, height=320, bg='white')
-	frmLC = Frame(width=500, height=150, bg='white')
-	frmLB = Frame(width=500, height=30)
-	frmRT = Frame(width=300, height=500)
+        self.text_frame = Frame(self.master, bd=6)
+        self.text_frame.pack(expand=True, fill=BOTH)
 
-	txtMsgList = Text(frmLT)
-	txtMsgList.tag_config('greencolor', foreground='#008C00')
-	txtMsg = Text(frmLC);
-	btnSend = Button(frmLB, text='Send', width = 8, command=sendMsg)
-	btnCancel = Button(frmLB, text='Cancel', width = 8, command=cancelMsg)
-	imgInfo = PhotoImage(file = "doctor.gif")
-	lblImage = Label(frmRT, image = imgInfo)
-	lblImage.image = imgInfo
+        # scrollbar for text box
+        self.text_box_scrollbar = Scrollbar(self.text_frame, bd=0)
+        self.text_box_scrollbar.pack(fill=Y, side=RIGHT)
 
-	frmLT.grid(row=0, column=0, columnspan=2, padx=1, pady=3)
-	frmLC.grid(row=1, column=0, columnspan=2, padx=1, pady=3)
-	frmLB.grid(row=2, column=0, columnspan=2)
-	frmRT.grid(row=0, column=2, rowspan=3, padx=2, pady=3)
-	frmLT.grid_propagate(0)
-	frmLC.grid_propagate(0)
-	frmLB.grid_propagate(0)
-	frmRT.grid_propagate(0)
-	btnSend.grid(row=2, column=0)
-	btnCancel.grid(row=2, column=1)
-	lblImage.grid()
-	txtMsgList.grid()
-	txtMsg.grid()
+        # contains messages
+        self.text_box = Text(self.text_frame, yscrollcommand=self.text_box_scrollbar.set, state=DISABLED,
+                             bd=1, padx=6, pady=6, spacing3=8, wrap=WORD, bg=None, font="Verdana 10", relief=GROOVE,
+                             width=10, height=1)
+        self.text_box.pack(expand=True, fill=BOTH)
+        self.text_box_scrollbar.config(command=self.text_box.yview)
 
-	t.mainloop()
+        # frame containing user entry field
+        self.entry_frame = Frame(self.master, bd=1)
+        self.entry_frame.pack(side=LEFT, fill=BOTH, expand=True)
+
+        # entry field
+        self.entry_field = Entry(self.entry_frame, bd=1, justify=LEFT)
+        self.entry_field.pack(fill=X, padx=6, pady=6, ipady=3)
+        # self.users_message = self.entry_field.get()
+
+        # frame containing send button and emoji button
+        self.send_button_frame = Frame(self.master, bd=0)
+        self.send_button_frame.pack(fill=BOTH)
+
+        # send button
+        self.send_button = Button(self.send_button_frame, text="Send", width=5, relief=GROOVE, bg='white',
+                                  bd=1, command=lambda: self.send_message_insert(None), activebackground="#FFFFFF",
+                                  activeforeground="#000000")
+        self.send_button.pack(side=LEFT, ipady=8)
+        self.master.bind("<Return>", self.send_message_insert)
+        
+        self.last_sent_label(date="No messages sent.")
+        #t2 = threading.Thread(target=self.send_message_insert(, name='t1')
+        #t2.start()
+        
+    def playResponce(self,response):
+        x=pyttsx3.init()
+        voices=x.getProperty('voices')
+        x.setProperty('voice',voices[2].id)
+        #print(responce)
+        li = []
+        if len(response) > 100:
+            if response.find('--') == -1:
+                b = response.split('--')
+                #print(b)
+                 
+        x.setProperty('rate',120)
+        # x.setProperty('volume',50)
+        x.say(response)
+        x.runAndWait()
+        #print("Played Successfully......")
+        
+        
+    def last_sent_label(self, date):
+
+        try:
+            self.sent_label.destroy()
+        except AttributeError:
+            pass
+
+        self.sent_label = Label(self.entry_frame, font="Verdana 7", text=date, bg=self.tl_bg2, fg=self.tl_fg)
+        self.sent_label.pack(side=LEFT, fill=X, padx=3)
+
+    def clear_chat(self):
+        self.text_box.config(state=NORMAL)
+        self.last_sent_label(date="No messages sent.")
+        self.text_box.delete(1.0, END)
+        self.text_box.delete(1.0, END)
+        self.text_box.config(state=DISABLED)
+
+    def chatexit(self):
+        exit()
+
+    def msg(self):
+        tkinter.messagebox.showinfo("Aarogya-Bot info")
+    def about(self):
+        tkinter.messagebox.showinfo("Developers")
+    
+    def send_message_insert(self, message):
+		# strMsg = message	
+        user_input = self.entry_field.get()
+        print(user_input)
+        pr1 = "Human : " + user_input + "\n"
+        self.text_box.configure(state=NORMAL)
+        self.text_box.insert(END, pr1)
+        self.text_box.configure(state=DISABLED)
+        self.text_box.see(END)
+        text2 = handler.chat_main(user_input) + '\n '
+        print(text2)
+        strMsg2 = 'Aarogya Bot: ' + text2 + '\n'
+        self.text_box.configure(state=NORMAL)
+        self.text_box.insert(END, strMsg2)
+        self.text_box.configure(state=DISABLED)
+        self.text_box.see(END)
+        self.last_sent_label(str(time.strftime( "Last message sent: " + '%B %d, %Y' + ' at ' + '%I:%M %p')))
+        self.entry_field.delete(0,END)
+        time.sleep(0)    
+        # ob=chat(user_input)
+        # pr="PyBot : " + ob + "\n"
+        # self.text_box.configure(state=NORMAL)
+
+        # self.text_box.configure(state=DISABLED)
+        # self.text_box.see(END)
+        # self.last_sent_label(str(time.strftime( "Last message sent: " + '%B %d, %Y' + ' at ' + '%I:%M %p')))
+        # self.entry_field.delete(0,END)
+        # time.sleep(0)
+        t2 = threading.Thread(target=self.playResponce, args=(text2, ))
+        t2.start()
+        #return ob
+
+    
+
+
+        
+        
+    def font_change_default(self):
+        self.text_box.config(font="Verdana 10")
+        self.entry_field.config(font="Verdana 10")
+        self.font = "Verdana 10"
+
+    def font_change_times(self):
+        self.text_box.config(font="Times")
+        self.entry_field.config(font="Times")
+        self.font = "Times"
+
+    def font_change_system(self):
+        self.text_box.config(font="System")
+        self.entry_field.config(font="System")
+        self.font = "System"
+
+    def font_change_helvetica(self):
+        self.text_box.config(font="helvetica 10")
+        self.entry_field.config(font="helvetica 10")
+        self.font = "helvetica 10"
+
+    def font_change_fixedsys(self):
+        self.text_box.config(font="fixedsys")
+        self.entry_field.config(font="fixedsys")
+        self.font = "fixedsys"
+
+    def color_theme_default(self):
+        self.master.config(bg="#EEEEEE")
+        self.text_frame.config(bg="#EEEEEE")
+        self.entry_frame.config(bg="#EEEEEE")
+        self.text_box.config(bg="#FFFFFF", fg="#000000")
+        self.entry_field.config(bg="#FFFFFF", fg="#000000", insertbackground="#000000")
+        self.send_button_frame.config(bg="#EEEEEE")
+        self.send_button.config(bg="#FFFFFF", fg="#000000", activebackground="#FFFFFF", activeforeground="#000000")
+        #self.emoji_button.config(bg="#FFFFFF", fg="#000000", activebackground="#FFFFFF", activeforeground="#000000")
+        self.sent_label.config(bg="#EEEEEE", fg="#000000")
+
+        self.tl_bg = "#FFFFFF"
+        self.tl_bg2 = "#EEEEEE"
+        self.tl_fg = "#000000"
+
+    # Dark
+    def color_theme_dark(self):
+        self.master.config(bg="#2a2b2d")
+        self.text_frame.config(bg="#2a2b2d")
+        self.text_box.config(bg="#212121", fg="#FFFFFF")
+        self.entry_frame.config(bg="#2a2b2d")
+        self.entry_field.config(bg="#212121", fg="#FFFFFF", insertbackground="#FFFFFF")
+        self.send_button_frame.config(bg="#2a2b2d")
+        self.send_button.config(bg="#212121", fg="#FFFFFF", activebackground="#212121", activeforeground="#FFFFFF")
+       # self.emoji_button.config(bg="#212121", fg="#FFFFFF", activebackground="#212121", activeforeground="#FFFFFF")
+        self.sent_label.config(bg="#2a2b2d", fg="#FFFFFF")
+
+        self.tl_bg = "#212121"
+        self.tl_bg2 = "#2a2b2d"
+        self.tl_fg = "#FFFFFF"
+
+    # Grey
+    def color_theme_grey(self):	
+        self.master.config(bg="#444444")
+        self.text_frame.config(bg="#444444")
+        self.text_box.config(bg="#4f4f4f", fg="#ffffff")
+        self.entry_frame.config(bg="#444444")
+        self.entry_field.config(bg="#4f4f4f", fg="#ffffff", insertbackground="#ffffff")
+        self.send_button_frame.config(bg="#444444")
+        self.send_button.config(bg="#4f4f4f", fg="#ffffff", activebackground="#4f4f4f", activeforeground="#ffffff")
+        #self.emoji_button.config(bg="#4f4f4f", fg="#ffffff", activebackground="#4f4f4f", activeforeground="#ffffff")
+        self.sent_label.config(bg="#444444", fg="#ffffff")
+
+        self.tl_bg = "#4f4f4f"
+        self.tl_bg2 = "#444444"
+        self.tl_fg = "#ffffff"
+
+
+        self.master.config(bg="#003333")
+        self.text_frame.config(bg="#003333")
+        self.text_box.config(bg="#669999", fg="#FFFFFF")
+        self.entry_frame.config(bg="#003333")
+        self.entry_field.config(bg="#669999", fg="#FFFFFF", insertbackground="#FFFFFF")
+        self.send_button_frame.config(bg="#003333")
+        self.send_button.config(bg="#669999", fg="#FFFFFF", activebackground="#669999", activeforeground="#FFFFFF")
+        #self.emoji_button.config(bg="#669999", fg="#FFFFFF", activebackground="#669999", activeforeground="#FFFFFF")
+        self.sent_label.config(bg="#003333", fg="#FFFFFF")
+
+        self.tl_bg = "#669999"
+        self.tl_bg2 = "#003333"
+        self.tl_fg = "#FFFFFF"    
+
+    # Blue
+    def color_theme_dark_blue(self):
+        self.master.config(bg="#263b54")
+        self.text_frame.config(bg="#263b54")
+        self.text_box.config(bg="#1c2e44", fg="#FFFFFF")
+        self.entry_frame.config(bg="#263b54")
+        self.entry_field.config(bg="#1c2e44", fg="#FFFFFF", insertbackground="#FFFFFF")
+        self.send_button_frame.config(bg="#263b54")
+        self.send_button.config(bg="#1c2e44", fg="#FFFFFF", activebackground="#1c2e44", activeforeground="#FFFFFF")
+        #self.emoji_button.config(bg="#1c2e44", fg="#FFFFFF", activebackground="#1c2e44", activeforeground="#FFFFFF")
+        self.sent_label.config(bg="#263b54", fg="#FFFFFF")
+
+        self.tl_bg = "#1c2e44"
+        self.tl_bg2 = "#263b54"
+        self.tl_fg = "#FFFFFF"
+
+ 
+    
+
+    # Torque
+    def color_theme_turquoise(self):
+        self.master.config(bg="#003333")
+        self.text_frame.config(bg="#003333")
+        self.text_box.config(bg="#669999", fg="#FFFFFF")
+        self.entry_frame.config(bg="#003333")
+        self.entry_field.config(bg="#669999", fg="#FFFFFF", insertbackground="#FFFFFF")
+        self.send_button_frame.config(bg="#003333")
+        self.send_button.config(bg="#669999", fg="#FFFFFF", activebackground="#669999", activeforeground="#FFFFFF")
+        #self.emoji_button.config(bg="#669999", fg="#FFFFFF", activebackground="#669999", activeforeground="#FFFFFF")
+        self.sent_label.config(bg="#003333", fg="#FFFFFF")
+
+        self.tl_bg = "#669999"
+        self.tl_bg2 = "#003333"
+        self.tl_fg = "#FFFFFF"
+
+    # Hacker
+    def color_theme_hacker(self):
+        self.master.config(bg="#0F0F0F")
+        self.text_frame.config(bg="#0F0F0F")
+        self.entry_frame.config(bg="#0F0F0F")
+        self.text_box.config(bg="#0F0F0F", fg="#33FF33")
+        self.entry_field.config(bg="#0F0F0F", fg="#33FF33", insertbackground="#33FF33")
+        self.send_button_frame.config(bg="#0F0F0F")
+        self.send_button.config(bg="#0F0F0F", fg="#FFFFFF", activebackground="#0F0F0F", activeforeground="#FFFFFF")
+        #self.emoji_button.config(bg="#0F0F0F", fg="#FFFFFF", activebackground="#0F0F0F", activeforeground="#FFFFFF")
+        self.sent_label.config(bg="#0F0F0F", fg="#33FF33")
+
+        self.tl_bg = "#0F0F0F"
+        self.tl_bg2 = "#0F0F0F"
+        self.tl_fg = "#33FF33"
+
+    
+
+    # Default font and color theme
+    def default_format(self):
+        self.font_change_default()
+        self.color_theme_default() 
+
+
 
 if __name__ == '__main__':
-	main()
+	root=Tk()
+	handler = ChatBotGraph() 
+	a = ChatInterface(root)
+	root.geometry(window_size)
+	root.title("Aarogya-Bot")
+	root.mainloop()
